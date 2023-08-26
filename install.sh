a2f73ce96c273e9e1eb4682588e4fe226dbcc976
Ben Hall <rcmast3r1@gmail.com>
2023-08-25 22:18:18 -0400

    removed install.sh

--- install.sh
+++
@@ -1,6 +0,0 @@
-    
-mkdir -p $out/lib
-    find . -maxdepth 2 -type f -name "*.a" -exec cp {} $out/lib \;
-    mkdir -p $out/include
-    find include -type f -name "*.h" -exec rsync --relative {} $out/include \;
-
